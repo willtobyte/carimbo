321a0f0fdd1933736adaa4f796675a30d3a96a74
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-05 09:21:13 -0300

    Work in progress

--- src/cursor.cpp
+++ src/cursor.cpp
@@ -81,6 +81,7 @@ void cursor::update(float_t) noexcept {
 }
 
 void cursor::draw() const noexcept {
+<<<<<<< HEAD
   const auto &animation = _animations.at(_action).keyframes[_frame];
   _spritesheet->draw(
       animation.frame,
