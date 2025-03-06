10934ecee9fe75e0607d42da843926c8eda93889
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-06 09:38:55 -0300

    Work in progress

--- src/scenemanager.cpp
+++ src/scenemanager.cpp
@@ -2,15 +2,13 @@
 
 using namespace framework;
 
-using json = nlohmann::json;
-
 scenemanager::scenemanager(std::shared_ptr<graphics::pixmappool> pixmappool) noexcept
     : _pixmappool(pixmappool) {}
 
 void scenemanager::set(const std::string &name) noexcept {
   _background.reset();
   const auto buffer = storage::io::read("scenes/" + name + ".json");
-  const auto j = json::parse(buffer);
+  const auto j = nlohmann::json::parse(buffer);
   _background = _pixmappool->get(j["background"].get_ref<const std::string &>());
   _size = {j.at("width").get<int32_t>(), j.at("height").get<int32_t>()};
 }
