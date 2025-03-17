6b1cd32075a30efe92e23b353c0b80a0f34083a2
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-17 10:17:22 -0300

    Work in progress

--- src/animation.hpp
+++ src/animation.hpp
@@ -9,11 +9,11 @@ namespace graphics {
 struct keyframe {
   geometry::rect frame;
   geometry::point offset;
-  uint64_t duration{};
+  uint64_t duration{0};
 };
 
 struct animation {
-  bool oneshot;
+  bool oneshot{false};
   std::optional<geometry::rect> hitbox;
   std::vector<keyframe> keyframes;
 };
