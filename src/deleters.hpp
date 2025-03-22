75ee8698dd23501dcd3b89f165f2904ad201992d
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-22 18:28:21 -0300

    Work in progress

--- src/deleters.hpp
+++ src/deleters.hpp
@@ -2,44 +2,6 @@
 
 #include "common.hpp"
 
-// struct SDL_Deleter {
-//   void operator()(SDL_Surface *ptr) const noexcept {
-//     if (ptr) {
-//       SDL_FreeSurface(ptr);
-//     }
-//   }
-
-//   void operator()(SDL_Texture *ptr) const noexcept {
-//     if (ptr) {
-//       SDL_DestroyTexture(ptr);
-//     }
-//   }
-
-//   void operator()(SDL_Renderer *ptr) const noexcept {
-//     if (ptr) {
-//       SDL_DestroyRenderer(ptr);
-//     }
-//   }
-
-//   void operator()(SDL_Window *ptr) const noexcept {
-//     if (ptr) {
-//       SDL_DestroyWindow(ptr);
-//     }
-//   }
-
-//   void operator()(SDL_RWops *ptr) const noexcept {
-//     if (ptr) {
-//       SDL_RWclose(ptr);
-//     }
-//   }
-
-//   void operator()(SDL_GameController *ptr) const noexcept {
-//     if (ptr) {
-//       SDL_GameControllerClose(ptr);
-//     }
-//   }
-// };
-
 template <typename T>
 inline constexpr bool always_false = false;
 
