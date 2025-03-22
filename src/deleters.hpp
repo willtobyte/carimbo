a8a390815cf6d14dfe8cfb9e259404155a151cf5
Rodrigo Delduca <rodrigo@delduca.org>
2025-03-22 16:08:05 -0300

    Work in progress

--- src/deleters.hpp
+++ src/deleters.hpp
@@ -47,18 +47,36 @@ struct SDL_Deleter {
   template <typename T>
   void operator()(T *ptr) const noexcept {
     if (ptr) {
-      if constexpr (std::is_same_v<T, SDL_Surface>) {
+      if constexpr (std::is_same_v<T, SDL_cond>) {
+        SDL_DestroyCond(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Cursor>) {
+        SDL_FreeCursor(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_GLContext>) {
+        SDL_GL_DeleteContext(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_GameController>) {
+        SDL_GameControllerClose(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Haptic>) {
+        SDL_HapticClose(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Joystick>) {
+        SDL_JoystickClose(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_mutex>) {
+        SDL_DestroyMutex(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Palette>) {
+        SDL_FreePalette(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_PixelFormat>) {
+        SDL_FreeFormat(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Renderer>) {
+        SDL_DestroyRenderer(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_RWops>) {
+        SDL_RWclose(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Semaphore>) {
+        SDL_DestroySemaphore(ptr);
+      } else if constexpr (std::is_same_v<T, SDL_Surface>) {
         SDL_FreeSurface(ptr);
       } else if constexpr (std::is_same_v<T, SDL_Texture>) {
         SDL_DestroyTexture(ptr);
-      } else if constexpr (std::is_same_v<T, SDL_Renderer>) {
-        SDL_DestroyRenderer(ptr);
       } else if constexpr (std::is_same_v<T, SDL_Window>) {
         SDL_DestroyWindow(ptr);
-      } else if constexpr (std::is_same_v<T, SDL_RWops>) {
-        SDL_RWclose(ptr);
-      } else if constexpr (std::is_same_v<T, SDL_GameController>) {
-        SDL_GameControllerClose(ptr);
       } else {
         static_assert(always_false<T>, "Unsupported SDL type for deletion.");
       }
