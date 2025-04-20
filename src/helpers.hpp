036e9a9d46ad22c51b49a917d68d16847119f275
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-20 11:59:05 -0300

    Work in progress

--- src/helpers.hpp
+++ src/helpers.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #define panic(...) do { \
-  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Fatal Error", fmt::format(__VA_ARGS__).c_str(), nullptr); \
+  const auto message = fmt::format(__VA_ARGS__); \
+  fmt::println(stderr, "{}", message); \
+  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Fatal Error", message.c_str(), nullptr); \
   std::abort(); \
 } while (0)
 
@@ -11,11 +13,13 @@
 
 namespace boost {
 BOOST_NORETURN inline void throw_exception(std::exception const& e) {
-  panic(e.what());
+  fmt::println(stderr, "{}", e.what());
+  std::abort();
 }
 
 BOOST_NORETURN inline void throw_exception(std::exception const& e, boost::source_location const&) {
-  panic(e.what());
+  fmt::println(stderr, "{}", e.what());
+  std::abort();
 }
 }
 #endif
