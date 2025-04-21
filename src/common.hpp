800c2beaa76fe8b15d463b49bdc0a70ce79f787f
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-21 11:07:24 -0300

    Work in progress

--- src/common.hpp
+++ src/common.hpp
@@ -4,19 +4,19 @@
 #define NOMINMAX
 #endif
 
-#define SOL_NO_EXCEPTIONS 1
-#define JSON_NOEXCEPTION 1
+#define SOL_NO_EXCEPTIONS
+
+#define JSON_NOEXCEPTION
 
 #ifdef DEBUG
-#define SOL_ALL_SAFETIES_ON 1
-#define SOL_PRINT_ERRORS 1
+#define SOL_ALL_SAFETIES_ON
 #endif
 
 #ifndef EMSCRIPTEN
-#define SOL_LUAJIT 1
+#define SOL_LUAJIT
 #endif
 
-#define WEBSOCKET 1
+#define WEBSOCKET
 
 #ifdef EMSCRIPTEN
 #include <emscripten.h>
@@ -29,7 +29,7 @@
 
 #ifndef EMSCRIPTEN
 #ifdef WEBSOCKET
-#define BOOST_NO_EXCEPTIONS 1
+#define BOOST_NO_EXCEPTIONS
 #include <boost/asio.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ssl.hpp>
