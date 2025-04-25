2f19ec7dafde614fb7aff59f33396da2e3158c1f
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-25 13:29:17 -0300

    Work in progress

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,7 +8,6 @@ renderer::renderer(SDL_Window *window) {
   SDL_SetNumberProperty(props, SDL_PROP_RENDERER_CREATE_PRESENT_VSYNC_NUMBER, 1);
   SDL_SetStringProperty(props, SDL_PROP_RENDERER_CREATE_NAME_STRING, nullptr);
 
-
   SDL_Renderer *renderer = SDL_CreateRendererWithProperties(props);
   if (!renderer) [[unlikely]] {
     throw std::runtime_error(fmt::format("[SDL_CreateRendererWithProperties] {}", SDL_GetError()));
