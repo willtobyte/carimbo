a417f7c6dbf27759b871293534227a717234658b
Rodrigo Delduca <rodrigo@delduca.org>
2025-05-15 11:00:47 -0300

    Work in progress

--- src/application.cpp
+++ src/application.cpp
@@ -24,11 +24,12 @@ using namespace framework;
         #endif
       #endif
 
-      fmt::println(stderr, "{}", error);
       #ifdef HAVE_BOOST
       boost::stacktrace::stacktrace st;
       fmt::println(stderr, "Stack trace:\n{}\n", boost::stacktrace::to_string(st));
       #endif
+
+      fmt::println(stderr, "{}", error);
 
       SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Ink Spill Catastrophe", error, nullptr);
     }
