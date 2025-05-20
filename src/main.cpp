4b106b4749e3cf8c04405a1eff6db2eb51c3e29d
Rodrigo Delduca <rodrigo@delduca.org>
2025-05-20 14:23:44 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include "application.hpp"
 
-const unsigned char null_terminated_file_data[] = {
-#embed "main.cpp"
-};
-
 int main(int argc, char **argv) {
   framework::application app(argc, std::move(argv));
 
