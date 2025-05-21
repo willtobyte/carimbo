6021f9e5c24a39cde96b0ab30b849846dd54d977
Rodrigo Delduca <rodrigo@delduca.org>
2025-05-21 17:37:04 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,7 @@ int main(int argc, char** argv) {
 #if defined(ANDROID)
 #include <android/native_activity.h>
 
-extern "C" __attribute__((visibility("default"))) void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
-  SDL_Android_Init(activity, savedState, savedStateSize);
-  SDL_SetMainReady();
-
+extern "C" __attribute__((visibility("default"))) void ANativeActivity_onCreate(ANativeActivity*, void*, size_t) {
   static constexpr int argc = 1;
   static constexpr char arg0[] = "android_app";
   static char* argv[] = { const_cast<char*>(arg0), nullptr };
