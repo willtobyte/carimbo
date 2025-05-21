14ebf03ff319e7a33ba66388b06bf2094929b758
Rodrigo Delduca <rodrigo@delduca.org>
2025-05-21 16:57:50 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,18 @@ int main(int argc, char** argv) {
   return app.run();
 }
 
-#if defined(ANDROID)
-#include <android/native_activity.h>
+// #if defined(ANDROID)
+// #include <android/native_activity.h>
 
-extern "C" __attribute__((visibility("default"))) void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
-  UNUSED(activity);
-  UNUSED(savedState);
-  UNUSED(savedStateSize);
+// extern "C" __attribute__((visibility("default"))) void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
+//   SDL_Android_Init(activity, savedState, savedStateSize);
+//   SDL_SetMainReady();
 
-  static constexpr int argc = 1;
-  static constexpr char arg0[] = "android_app";
-  static char* argv[] = { const_cast<char*>(arg0), nullptr };
+//   static constexpr int argc = 1;
+//   static constexpr char arg0[] = "android_app";
+//   static char* argv[] = { const_cast<char*>(arg0), nullptr };
 
-  framework::application app(argc, argv);
-  app.run();
-}
-#endif
+//   framework::application app(argc, argv);
+//   app.run();
+// }
+// #endif
