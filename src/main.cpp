b814401cca067d855e2d53ec5cbb6f4624060a04
Rodrigo Delduca <rodrigo@delduca.org>
2025-05-21 13:13:11 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include "application.hpp"
 
-int main(int argc, char **argv) {
-  framework::application app(argc, std::move(argv));
-
+int main(int argc, char** argv) {
+  framework::application app(argc, argv);
   return app.run();
 }
+
+#if defined(__ANDROID__)
+#include <android/native_activity.h>
+
+extern "C" void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
+  UNUSED(activity);
+  UNUSED(savedState);
+  UNUSED(savedStateSize);
+
+  static constexpr int argc = 1;
+  static constexpr char arg0[] = "android_app";
+  static const auto* argv[] = { const_cast<char*>(arg0), nullptr };
+
+  main(argc, argv);
+}
+#endif
