e8ab6f86a0135e446c041b9a62897074bdced2d8
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-01 15:32:19 -0300

    Work in progress

--- src/steam.hpp
+++ src/steam.hpp
@@ -1,3 +1,11 @@
 #pragma once
 
 #include "common.hpp"
+
+bool SteamAPI_Init();
+void SteamAPI_Shutdown();
+void SteamAPI_RunCallbacks();
+void* SteamUserStats();
+bool GetAchievement(const char* name);
+bool SetAchievement(const char* name);
+bool StoreStats();
