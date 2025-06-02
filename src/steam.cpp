8b3e247c962e23e0557f9081b3fc7d9b15dc0164
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-02 09:20:06 -0300

    Work in progress

--- src/steam.cpp
+++ src/steam.cpp
@@ -44,13 +44,13 @@ void SteamAPI_RunCallbacks() {
 
   const auto address = GetProcAddress(hSteamApi, "SteamAPI_RunCallbacks");
   if (!address) {
-    fmt::println("Not address SteamAPI_RunCallbacks")
+    fmt::println("Not address SteamAPI_RunCallbacks");
     return;
   }
 
   using SteamAPI_RunCallbacks_t = void(S_CALLTYPE *)();
   reinterpret_cast<SteamAPI_RunCallbacks_t>(address)();
-  fmt::println(">>> SteamAPI_RunCallbacks")
+  fmt::println(">>> SteamAPI_RunCallbacks");
 }
 
 // ---
