1ab5782ed307241de92493c8c1c412e2d6d6a815
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-02 14:32:22 -0300

    Work in progress

--- src/steam.cpp
+++ src/steam.cpp
@@ -9,106 +9,62 @@
 
 static const HMODULE hSteamApi = LoadLibraryA("steam_api64.dll");
 
+using SteamAPI_InitSafe_t = bool(S_CALLTYPE *)();
+using SteamAPI_Shutdown_t = void(S_CALLTYPE *)();
+using SteamAPI_RunCallbacks_t = void(S_CALLTYPE *)();
+using SteamUserStats_t = void*(S_CALLTYPE *)();
+using GetAchievement_t = bool(S_CALLTYPE *)(void*, const char*, bool*);
+using SetAchievement_t = bool(S_CALLTYPE *)(void*, const char*);
+using StoreStats_t = bool(S_CALLTYPE *)(void*);
+
+static const SteamAPI_InitSafe_t pSteamAPI_InitSafe =
+  reinterpret_cast<SteamAPI_InitSafe_t>(GetProcAddress(hSteamApi, "SteamAPI_InitSafe"));
+
+static const SteamAPI_Shutdown_t pSteamAPI_Shutdown =
+  reinterpret_cast<SteamAPI_Shutdown_t>(GetProcAddress(hSteamApi, "SteamAPI_Shutdown"));
+
+static const SteamAPI_RunCallbacks_t pSteamAPI_RunCallbacks =
+  reinterpret_cast<SteamAPI_RunCallbacks_t>(GetProcAddress(hSteamApi, "SteamAPI_RunCallbacks"));
+
+static const SteamUserStats_t pSteamUserStats =
+  reinterpret_cast<SteamUserStats_t>(GetProcAddress(hSteamApi, "SteamAPI_SteamUserStats_v013"));
+
+static const GetAchievement_t pGetAchievement =
+  reinterpret_cast<GetAchievement_t>(GetProcAddress(hSteamApi, "SteamAPI_ISteamUserStats_GetAchievement"));
+
+static const SetAchievement_t pSetAchievement =
+  reinterpret_cast<SetAchievement_t>(GetProcAddress(hSteamApi, "SteamAPI_ISteamUserStats_SetAchievement"));
+
+static const StoreStats_t pStoreStats =
+  reinterpret_cast<StoreStats_t>(GetProcAddress(hSteamApi, "SteamAPI_ISteamUserStats_StoreStats"));
+
 bool SteamAPI_InitSafe() {
-  if (!hSteamApi) {
-    return true;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_InitSafe");
-  if (!address) {
-    return false;
-  }
-
-  using SteamAPI_InitSafe_t = bool(S_CALLTYPE *)();
-  return reinterpret_cast<SteamAPI_InitSafe_t>(address)();
+  return pSteamAPI_InitSafe();
 }
 
 void SteamAPI_Shutdown() {
-  if (!hSteamApi) {
-    return;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_Shutdown");
-  if (!address) {
-    return;
-  }
-
-  using SteamAPI_Shutdown_t = void(S_CALLTYPE *)();
-  reinterpret_cast<SteamAPI_Shutdown_t>(address)();
+  pSteamAPI_Shutdown();
 }
 
 void SteamAPI_RunCallbacks() {
-  if (!hSteamApi) {
-    return;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_RunCallbacks");
-  if (!address) {
-    return;
-  }
-
-  using SteamAPI_RunCallbacks_t = void(S_CALLTYPE *)();
-  reinterpret_cast<SteamAPI_RunCallbacks_t>(address)();
+  pSteamAPI_RunCallbacks();
 }
 
 void* SteamUserStats() {
-  if (!hSteamApi) {
-    return nullptr;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_SteamUserStats_v013");
-  if (!address) {
-    return nullptr;
-  }
-
-  using SteamUserStats_t = void*(S_CALLTYPE *)();
-  return reinterpret_cast<SteamUserStats_t>(address)();
+  return pSteamUserStats();
 }
 
 bool GetAchievement(const char* name) {
-  const auto stats = SteamUserStats();
-  if (!stats) {
-    return false;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_ISteamUserStats_GetAchievement");
-  if (!address) {
-    return false;
-  }
-
-  using GetAchievement_t = bool(S_CALLTYPE *)(void*, const char*, bool*);
   bool achieved = false;
-  return reinterpret_cast<GetAchievement_t>(address)(stats, name, &achieved) && achieved;
+  return pGetAchievement(SteamUserStats(), name, &achieved) && achieved;
 }
 
 bool SetAchievement(const char* name) {
-  const auto stats = SteamUserStats();
-  if (!stats) {
-    return false;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_ISteamUserStats_SetAchievement");
-  if (!address) {
-    return false;
-  }
-
-  using SetAchievement_t = bool(S_CALLTYPE *)(void*, const char*);
-  return reinterpret_cast<SetAchievement_t>(address)(stats, name);
+  return pSetAchievement(SteamUserStats(), name);
 }
 
 bool StoreStats() {
-  const auto stats = SteamUserStats();
-  if (!stats) {
-    return false;
-  }
-
-  const auto address = GetProcAddress(hSteamApi, "SteamAPI_ISteamUserStats_StoreStats");
-  if (!address) {
-    return false;
-  }
-
-  using StoreStats_t = bool(S_CALLTYPE *)(void*);
-  return reinterpret_cast<StoreStats_t>(address)(stats);
+  return pStoreStats(SteamUserStats());
 }
 
 #else
