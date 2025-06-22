642ae57c5c146c9c38a37bbcb4bcf91a05885d36
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-22 08:55:14 -0300

    Work in progress

--- src/objectpool.hpp
+++ src/objectpool.hpp
@@ -3,89 +3,79 @@
 #include "common.hpp"
 
 #include "singleton.hpp"
+
 #include "envelope.hpp"
 #include "object.hpp"
 
 namespace framework {
+
 template<typename T, typename PtrType>
 class poolbase {
 protected:
-  std::vector<PtrType> objects;
+  std::vector<PtrType> _objects;
 
   template<typename... Args>
   void expand(size_t minimum) {
-    const auto target = std::max(minimum, objects.empty() ? size_t(1) : objects.size() * 2);
+    const size_t target = std::max(minimum, _objects.empty() ? size_t(1) : _objects.size() * 2);
+    _objects.reserve(target);
 
-    for (auto i = objects.size(); i < target; ++i) {
-      objects.emplace_back(std::make_unique<T>());
+    for (size_t i = _objects.size(); i < target; ++i) {
+      _objects.emplace_back(std::make_unique<T>());
     }
   }
 
 public:
   template<typename... Args>
-  PtrType acquire(Args&&... arguments) {
-    if (objects.empty()) {
+  PtrType acquire(Args&&... args) {
+    if (_objects.empty()) {
       expand(size());
     }
 
-    PtrType object = std::move(objects.back());
-    objects.pop_back();
+    PtrType object = std::move(_objects.back());
+    _objects.pop_back();
 
-    if constexpr (requires { object->reset(std::forward<Args>(arguments)...); }) {
-      object->reset(std::forward<Args>(arguments)...);
+    if constexpr (requires { object->reset(std::forward<Args>(args)...); }) {
+      object->reset(std::forward<Args>(args)...);
     }
 
     return object;
   }
 
   void release(PtrType object) {
+    if (!object) {
+      return;
+    }
+
     if constexpr (std::is_same_v<PtrType, std::shared_ptr<T>>) {
       if (object.use_count() != MINIMAL_USE_COUNT) {
         return;
       }
     }
 
-    objects.push_back(std::move(object));
+    _objects.push_back(std::move(object));
   }
 
-  template<typename... Args>
-  void reserve(size_t count, Args&&... default_args) {
-    expand(count, std::forward<Args>(default_args)...);
+  void reserve(size_t count) {
+    expand(count);
   }
 
   size_t size() const {
-    return objects.size();
+    return _objects.size();
   }
 };
 
 template<typename T>
 class sharedpool : public poolbase<T, std::shared_ptr<T>> {
 public:
-  using PtrType = std::shared_ptr<T>;
-
-  template<typename... Args>
-  PtrType acquire(Args&&... arguments) {
-    return poolbase<T, PtrType>::acquire(std::forward<Args>(arguments)...);
-  }
-
-  void release(PtrType object) {
-    poolbase<T, PtrType>::release(std::move(object));
-  }
+  using poolbase<T, std::shared_ptr<T>>::acquire;
+  using poolbase<T, std::shared_ptr<T>>::release;
 };
 
 template<typename T>
 class uniquepool : public poolbase<T, std::unique_ptr<T>> {
 public:
-  using PtrType = std::unique_ptr<T>;
-
-  template<typename... Args>
-  PtrType acquire(Args&&... arguments) {
-    return poolbase<T, PtrType>::acquire(std::forward<Args>(arguments)...);
-  }
-
-  void release(PtrType object) {
-    poolbase<T, PtrType>::release(std::move(object));
-  }
+  using poolbase<T, std::unique_ptr<T>>::acquire;
+  using poolbase<T, std::unique_ptr<T>>::release;
 };
 
 using envelopepool = singleton<uniquepool<envelope>>;
