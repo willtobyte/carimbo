a3e2824e84ec6103ed20e9d27cfa6db55adcb0e5
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-22 11:32:53 -0300

    Work in progress

--- src/objectpool.hpp
+++ src/objectpool.hpp
@@ -17,6 +17,7 @@ protected:
   template<typename... Args>
   void expand(size_t minimum) {
     const size_t target = std::max(minimum, _objects.empty() ? size_t(1) : _objects.size() * 2);
+
     _objects.reserve(target);
 
     for (size_t i = _objects.size(); i < target; ++i) {
