bf7fbe55bb303c99d2ea1ee9f995b2deb921f2a5
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-23 11:26:00 -0300

    Work in progress

--- src/objectmanager.cpp
+++ src/objectmanager.cpp
@@ -156,7 +156,7 @@ void objectmanager::set_scenemanager(std::shared_ptr<scenemanager> scenemanager)
 }
 
 void objectmanager::update(float_t dt) noexcept {
-  for (auto itoa = _objects.begin(); itoa != _objects.end(); ++itoa) {
+  for (const auto itoa = _objects.begin(); itoa != _objects.end(); ++itoa) {
     auto& a = *itoa;
     a->update(dt);
 
@@ -171,7 +171,7 @@ void objectmanager::update(float_t dt) noexcept {
       ha.rectangle.size() * a->_scale
     );
 
-    for (auto itob = std::next(itoa); itob != _objects.end(); ++itob) {
+    for (const auto itob = std::next(itoa); itob != _objects.end(); ++itob) {
       auto& b = *itob;
 
       const auto itb = b->_animations.find(b->_action);
