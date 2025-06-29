acc250f51eb9d1e8f759f9d29c958a4a037ebab4
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-29 12:41:40 -0300

    Work in progress

--- src/objectmanager.cpp
+++ src/objectmanager.cpp
@@ -77,7 +77,7 @@ std::shared_ptr<object> objectmanager::create(const std::string& kind, std::opti
   o->_animations = std::move(animations);
   o->_spritesheet = std::move(spritesheet);
 
-  std::println("[objectmanager] created {} {}", qualifier, o->id());
+  fmt::println("[objectmanager] created {} {}", qualifier, o->id());
   if (manage) {
     _objects.emplace_back(o);
   }
