da3fb5c78ff6c5c6eef6d9872eedcdd12f0ef711
Rodrigo Delduca <rodrigo@delduca.org>
2025-08-06 15:32:59 -0300

    Work in progress

--- src/statemanager.cpp
+++ src/statemanager.cpp
@@ -62,6 +62,7 @@ void statemanager::on_key_release(const keyboard::key& event) {
 }
 
 void statemanager::on_gamepad_press(uint8_t who, const gamepad::button& event) {
+  std::println("on_gamepad_press {}", who);
   _state[who - 1][event] = true;
 }
 
