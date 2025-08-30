880c6900cf35e022ca016013f6ab8da5048b9d54
Rodrigo Delduca <rodrigo@delduca.org>
2025-08-30 13:36:19 -0300

    Work in progress

--- src/io.cpp
+++ src/io.cpp
@@ -5,6 +5,8 @@ using namespace storage;
 std::vector<std::uint8_t> io::read(std::string_view filename) {
   auto ptr = std::unique_ptr<PHYSFS_File, decltype(&PHYSFS_close)>(PHYSFS_openRead(filename.data()), PHYSFS_close);
 
+  PHYSFS_setBuffer(ptr.get(), 4 * 1024 * 1024);
+
   if (!ptr) [[unlikely]] {
     throw std::runtime_error(
       std::format("[PHYSFS_openRead] error while opening file: {}, error: {}",
