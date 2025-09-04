5ec7420dee8f23c843402314d3957e58945fab8b
Rodrigo Delduca <rodrigo@delduca.org>
2025-09-04 18:37:20 -0300

    Work in progress

--- src/timermanager.cpp
+++ src/timermanager.cpp
@@ -46,7 +46,7 @@ void timermanager::clear(uint32_t id) {
 }
 
 uint32_t timermanager::add_timer(uint32_t interval, std::function<void()>&& fn, bool repeat) {
-  auto ptr = _envelopepool->acquire(timerenvelope(repeat, std::move(fn))).release();
+  const auto ptr = _envelopepool->acquire(timerenvelope(repeat, std::move(fn))).release();
 
   const auto id = SDL_AddTimer(interval, repeat ? wrapper : singleshot_wrapper, ptr);
   if (id) [[likely]] {
