27ad3ae65df71d13710a525357d53a0d9307851a
Rodrigo Delduca <rodrigo@delduca.org>
2025-09-12 11:27:18 -0300

    Work in progress

--- src/point.cpp
+++ src/point.cpp
@@ -42,8 +42,8 @@ point& point::operator+=(const point& other) noexcept {
 }
 
 point& point::operator+=(const std::pair<uint8_t, float_t>& o) noexcept {
-  static constexpr const auto X = static_cast<uint8_t>('x');
-  static constexpr const auto Y = static_cast<uint8_t>('y');
+  constexpr const auto X = static_cast<uint8_t>('x');
+  constexpr const auto Y = static_cast<uint8_t>('y');
 
   switch (o.first) {
     case X: _x += o.second; return *this;
