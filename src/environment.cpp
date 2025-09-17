79e6e22891fd250d7c6e55d7040b929fefaacead
Rodrigo Delduca <rodrigo@delduca.org>
2025-09-17 12:00:03 -0300

    Work in progress

--- src/environment.cpp
+++ src/environment.cpp
@@ -3,7 +3,7 @@
 using namespace platform;
 
 std::optional<std::string> desktop::folder() const noexcept {
-  const char* folder = SDL_GetUserFolder(SDL_FOLDER_DESKTOP);
+  const auto* folder = SDL_GetUserFolder(SDL_FOLDER_DESKTOP);
   if (!folder) {
     return std::nullopt;
   }
