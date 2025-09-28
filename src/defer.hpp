268ae0cc7ec296b57c74d4d65113cc44dc98bbf1
Rodrigo Delduca <rodrigo@delduca.org>
2025-09-28 15:25:59 -0300

    Work in progress

--- src/defer.hpp
+++ src/defer.hpp
@@ -1,24 +1,24 @@
 #pragma once
 
 template <typename F>
-struct Defer {
+struct _defer {
   [[no_unique_address]] F f;
 
-  explicit Defer(F&& fn) noexcept(std::is_nothrow_move_constructible_v<F>)
+  explicit _defer(F&& fn) noexcept(std::is_nothrow_move_constructible_v<F>)
       : f(std::move(fn)) {}
 
-  Defer(const Defer&) = delete;
-  Defer& operator=(const Defer&) = delete;
-  Defer(Defer&&) = delete;
+  _defer(const _defer&) = delete;
+  _defer& operator=(const _defer&) = delete;
+  _defer(_defer&&) = delete;
 
-  ~Defer() noexcept(noexcept(std::declval<F&>()())) {
+  ~_defer() noexcept(noexcept(std::declval<F&>()())) {
     f();
   }
 };
 
 template <typename F>
 [[nodiscard]] auto make_defer(F&& f) {
-  return Defer<std::decay_t<F>>(std::forward<F>(f));
+  return _defer<std::decay_t<F>>(std::forward<F>(f));
 }
 
 #define defer(code) auto _defer_##__LINE__ = make_defer([&](){ code; })
