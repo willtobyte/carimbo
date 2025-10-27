50dc21c58ed76646743cfbfda020f86319550b09
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-27 10:41:13 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -16,16 +16,3 @@ int main(int argc, char **argv) {
   framework::application app(argc, argv);
   return app.run();
 }
-
-#ifdef ANDROID
-#include <android/native_activity.h>
-
-extern "C" __attribute__((visibility("default"))) void ANativeActivity_onCreate(ANativeActivity*, void*, size_t) {
-  static constexpr int argc = 1;
-  static constexpr char arg0[] = "android_app";
-  static char* argv[] = { const_cast<char*>(arg0), nullptr };
-
-  framework::application app(argc, argv);
-  app.run();
-}
-#endif
