f44629388ef588649d4c72911f605951aefea2b2
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-27 10:34:24 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 
 int main(int argc, char **argv) {
   #ifndef DEBUG
+  #ifndef EMSCRIPTEN
   auto* out = std::freopen("stdout.txt", "w", stdout);
   if (out) setvbuf(out, nullptr, _IONBF, 0);
 
   auto* err = std::freopen("stderr.txt", "w", stderr);
   if (err) setvbuf(err, nullptr, _IONBF, 0);
+  #endif
   #endif
 
   framework::application app(argc, argv);
