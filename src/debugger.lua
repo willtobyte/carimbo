6a2db9198ba3265d75a375d109eb98e99b248396
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-30 21:20:37 -0300

    Work in progress

--- src/debugger.lua
+++ src/debugger.lua
@@ -424,7 +424,7 @@ local function cmd_where(context_lines)
   local info = debug.getinfo(stack_inspect_offset + CMD_STACK_LEVEL)
   return (info and where(info, tonumber(context_lines) or 5))
 end
-)lua";
+)lua"
 R"lua(
 local function cmd_trace()
   dbg_writeln("Inspecting frame %d", stack_inspect_offset - stack_top)
