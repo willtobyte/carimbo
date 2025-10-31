5c56812601da2d4b35bf57feff47919ae979841f
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-31 18:55:04 -0300

    Work in progress

--- src/bootstrap.lua
+++ src/bootstrap.lua
@@ -1,16 +1,16 @@
 R"lua(
 local state = {}
 
-local function wrap(k)
+local function _wrap_key(k)
   return scenemanager.current.name .. '/' .. k
 end
 
 setmetatable(state, {
   __newindex = function(t, k, v)
-    cassette:set(wrap(k), v)
+    cassette:set(_wrap_key(k), v)
   end,
   __index = function(t, k)
-    return cassette:get(wrap(k), nil)
+    return cassette:get(_wrap_key(k), nil)
   end,
 })
 
