20441bb3fd6bad7ded96fe4213bc78d2cac838e9
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-01 12:24:30 -0300

    Work in progress

--- src/bootstrap.lua
+++ src/bootstrap.lua
@@ -31,20 +31,4 @@ setmetatable(state, {
 })
 
 _G.state = state
-
-function transition(options)
-  -- Destroy scenes first
-  if options.destroy then
-    for _, name in ipairs(options.destroy) do
-      scenemanager:destroy(name)
-    end
-  end
-
-  -- Register new scenes
-  if options.register then
-    for _, name in ipairs(options.register) do
-      scenemanager:register(name)
-    end
-  end
-end
 )lua";
