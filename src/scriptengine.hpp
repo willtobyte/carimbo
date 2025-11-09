a9df591ce07c77de9a841dadd759f06e6a8cfafc
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-09 16:21:16 -0300

    Work in progress

--- src/scriptengine.hpp
+++ src/scriptengine.hpp
@@ -1,20 +1,47 @@
 #pragma once
 
 #include "common.hpp"
+#include <concepts>
+#include <type_traits>
 
 namespace interop {
+
+template<typename Signature>
+struct wrap_fn_impl;
+
 template<typename... Args>
-static auto wrap_fn(sol::protected_function pf) -> std::function<void(Args...)> {
-  return [pf = std::move(pf)](Args... args) mutable {
-    auto result = pf(std::forward<Args>(args)...);
-    if (!result.valid()) [[unlikely]] {
-      sol::error error = result;
-      throw std::runtime_error(error.what());
-    }
-  };
+struct wrap_fn_impl<void(Args...)> {
+  static auto wrap(sol::protected_function pf) -> std::function<void(Args...)> {
+    return [pf = std::move(pf)](Args... args) mutable {
+      auto result = pf(std::forward<Args>(args)...);
+      if (!result.valid()) [[unlikely]] {
+        sol::error error = result;
+        throw std::runtime_error(error.what());
+      }
+    };
+  }
+};
+
+template<typename ReturnType, typename... Args>
+struct wrap_fn_impl<ReturnType(Args...)> {
+  static auto wrap(sol::protected_function pf) -> std::function<ReturnType(Args...)> {
+    return [pf = std::move(pf)](Args... args) mutable -> ReturnType {
+      auto result = pf(std::forward<Args>(args)...);
+      if (!result.valid()) [[unlikely]] {
+        sol::error error = result;
+        throw std::runtime_error(error.what());
+      }
+      return result.template get<ReturnType>();
+    };
+  }
+};
+
+template<typename Signature>
+auto wrap_fn(sol::protected_function pf) -> std::function<Signature> {
+  return wrap_fn_impl<Signature>::wrap(std::move(pf));
 }
 
-static auto wrap_fn(sol::protected_function pf) {
+auto wrap_fn(sol::protected_function pf) {
   return [pf = std::move(pf)](auto&&... args) mutable {
     auto result = pf(std::forward<decltype(args)>(args)...);
     if (!result.valid()) [[unlikely]] {
