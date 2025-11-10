d0e415e2502dc454529bb46430a62d2a9942a8fa
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-10 13:25:48 -0300

    Work in progress

--- src/constant.hpp
+++ src/constant.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
-inline constexpr auto MINIMAL_USE_COUNT = 1L;
+static constexpr auto MINIMAL_USE_COUNT = 1L;
 
-inline constexpr auto WORLD_SUBSTEPS = 4;
+static constexpr auto WORLD_SUBSTEPS = 4;
 
-inline constexpr float FIXED_TIMESTEP = 1.0f / 60.0f;
+static constexpr float FIXED_TIMESTEP = 1.0f / 60.0f;
 
-inline constexpr auto FLUSH_INTERVAL = 10000u;
+static constexpr auto FLUSH_INTERVAL = 10000u;
 
-inline constexpr float DEGREES_TO_RADIANS = std::numbers::pi_v<float> / 180.0f;
+static constexpr auto DEGREES_TO_RADIANS = std::numbers::pi_v<float> / 180.0f;
 
-inline constexpr float RADIANS_TO_DEGREES = 180.0f / std::numbers::pi_v<float>;
+static constexpr auto RADIANS_TO_DEGREES = 180.0f / std::numbers::pi_v<float>;
 
-inline constexpr auto epsilon = std::numeric_limits<float>::epsilon();
+static constexpr auto epsilon = std::numeric_limits<float>::epsilon();
