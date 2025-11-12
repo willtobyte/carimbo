eb55be57973b8c2d09657bbaeda78531504cd1e5
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-12 08:16:33 -0300

    Work in progress

--- src/achievement.hpp
+++ src/achievement.hpp
@@ -3,11 +3,11 @@
 #include "common.hpp"
 
 namespace steam {
-  class achievement final {
-  public:
-      achievement() = default;
-      ~achievement() = default;
+class achievement final {
+public:
+  achievement() = default;
+  ~achievement() = default;
 
-      void unlock(const std::string& id);
-  };
+  void unlock(const std::string& id);
+};
 }
