7cf86278b8c4fd0fad44e64b2ead8c7ee6e24fac
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-21 14:02:18 -0300

    Work in progress

--- src/formatters.hpp
+++ src/formatters.hpp
@@ -14,57 +14,74 @@ struct std::formatter<geometry::point> {
   }
 
   auto format(const geometry::point& p, std::format_context& context) const {
-    return std::format_to(context.out(), "point({}, {})", p.x(), p.y());
+    auto out = context.out();
+    out = std::format_to(out, "point({}, {})", p.x(), p.y());
+    return out;
   }
 };
 
 template<>
-struct std::formatter<geometry::rectangle> : std::formatter<std::string> {
+struct std::formatter<geometry::rectangle> {
+  constexpr auto parse(std::format_parse_context& context) {
+    return context.begin();
+  }
+
   auto format(const geometry::rectangle& r, std::format_context& context) const {
-    return std::formatter<std::string>::format(
-      std::format("rectangle({}, {}, {}x{})", r.x(), r.y(), r.width(), r.height()),
-      context
-    );
+    auto out = context.out();
+    out = std::format_to(out, "rectangle({}, {}, {}x{})", r.x(), r.y(), r.width(), r.height());
+    return out;
   }
 };
 
 template<>
-struct std::formatter<geometry::size> : std::formatter<std::string> {
+struct std::formatter<geometry::size> {
+  constexpr auto parse(std::format_parse_context& context) {
+    return context.begin();
+  }
+
   auto format(const geometry::size& s, std::format_context& context) const {
-    return std::formatter<std::string>::format(
-      std::format("size({}x{})", s.width(), s.height()),
-      context
-    );
+    auto out = context.out();
+    out = std::format_to(out, "size({}x{})", s.width(), s.height());
+    return out;
   }
 };
 
 template<>
-struct std::formatter<geometry::margin> : std::formatter<std::string> {
+struct std::formatter<geometry::margin> {
+  constexpr auto parse(std::format_parse_context& context) {
+    return context.begin();
+  }
+
   auto format(const geometry::margin& m, std::format_context& context) const {
-    return std::formatter<std::string>::format(
-      std::format("margin(t:{}, l:{}, b:{}, r:{})", m.top(), m.left(), m.bottom(), m.right()),
-      context
-    );
+    auto out = context.out();
+    out = std::format_to(out, "margin(t:{}, l:{}, b:{}, r:{})", m.top(), m.left(), m.bottom(), m.right());
+    return out;
   }
 };
 
 template<>
-struct std::formatter<graphics::color> : std::formatter<std::string> {
+struct std::formatter<graphics::color> {
+  constexpr auto parse(std::format_parse_context& context) {
+    return context.begin();
+  }
+
   auto format(const graphics::color& c, std::format_context& context) const {
-    return std::formatter<std::string>::format(
-      std::format("color(#{:02x}{:02x}{:02x}{:02x})", c.r(), c.g(), c.b(), c.a()),
-      context
-    );
+    auto out = context.out();
+    out = std::format_to(out, "color(#{:02x}{:02x}{:02x}{:02x})", c.r(), c.g(), c.b(), c.a());
+    return out;
   }
 };
 
 template<>
-struct std::formatter<framework::object> : std::formatter<std::string> {
+struct std::formatter<framework::object> {
+  constexpr auto parse(std::format_parse_context& context) {
+    return context.begin();
+  }
+
   auto format(const framework::object& o, std::format_context& context) const {
-    return std::formatter<std::string>::format(
-      std::format("object(placement:{}, angle:{}, alpha:{}, visible:{}, action:{})",
-        o.placement(), o.angle(), o.alpha(), o.visible(), o.action()),
-      context
-    );
+    auto out = context.out();
+    out = std::format_to(out, "object(placement:{}, angle:{}, alpha:{}, visible:{}, action:{})", 
+      o.placement(), o.angle(), o.alpha(), o.visible(), o.action());
+    return out;
   }
 };
