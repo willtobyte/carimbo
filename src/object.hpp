06fb74f9a92e47dc772c9bde1bd9fba7e31cea0d
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-21 13:14:28 -0300

    Work in progress

--- src/object.hpp
+++ src/object.hpp
@@ -177,12 +177,12 @@ private:
 
 template<>
 struct std::formatter<framework::object> {
-  constexpr auto parse(std::format_parse_context& ctx) {
-    return ctx.begin();
+  constexpr auto parse(std::format_parse_context& context) {
+    return context.begin();
   }
 
-  auto format(const framework::object& o, std::format_context& ctx) const {
-    return std::format_to(ctx.out(),
+  auto format(const framework::object& o, std::format_context& context) const {
+    return std::format_to(context.out(),
       "object(placement:{}, angle:{}, alpha:{}, visible:{}, action:{})",
       o.placement(), o.angle(), o.alpha(), o.visible(), o.action());
   }
