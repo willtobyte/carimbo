d41dccd7ee544f096629a62fb742af9af482a5ff
Rodrigo Delduca <rodrigo@delduca.org>
2025-11-23 15:03:54 -0300

    Work in progress

---
+++ src/vector.hpp
@@ -0,0 +1,237 @@
+#pragma once
+
+#include "common.hpp"
+
+namespace geometry {
+
+template <std::size_t N>
+class vector {
+public:
+  constexpr vector() noexcept = default;
+
+  constexpr explicit vector(std::array<float, N> const& data) noexcept
+      : _data(data) {}
+
+  template <typename... Args>
+      requires (sizeof...(Args) == N) && (std::same_as<Args, float> && ...)
+  constexpr vector(Args... args) noexcept
+      : _data{static_cast<float>(args)...} {}
+
+  [[nodiscard]] constexpr float operator[](std::size_t i) const noexcept {
+    return _data[i];
+  }
+
+  [[nodiscard]] constexpr float& operator[](std::size_t i) noexcept {
+    return _data[i];
+  }
+
+private:
+  std::array<float, N> _data{};
+};
+
+template <>
+class vector<2> {
+public:
+  constexpr vector() noexcept = default;
+
+  constexpr vector(float x, float y) noexcept
+      : _data{x, y} {}
+
+  constexpr explicit vector(std::array<float, 2> const& data) noexcept
+      : _data(data) {}
+
+  [[nodiscard]] constexpr float x() const noexcept { return _data[0]; }
+  [[nodiscard]] constexpr float y() const noexcept { return _data[1]; }
+
+  [[nodiscard]] constexpr float& x() noexcept { return _data[0]; }
+  [[nodiscard]] constexpr float& y() noexcept { return _data[1]; }
+
+  [[nodiscard]] constexpr float operator[](std::size_t i) const noexcept {
+    return _data[i];
+  }
+
+  [[nodiscard]] constexpr float& operator[](std::size_t i) noexcept {
+    return _data[i];
+  }
+
+private:
+  std::array<float, 2> _data{};
+};
+
+template <>
+class vector<4> {
+public:
+  constexpr vector() noexcept = default;
+
+  constexpr vector(float x, float y, float w, float h) noexcept
+      : _data{x, y, w, h} {}
+
+  constexpr explicit vector(std::array<float, 4> const& data) noexcept
+      : _data(data) {}
+
+  [[nodiscard]] constexpr float x() const noexcept { return _data[0]; }
+  [[nodiscard]] constexpr float y() const noexcept { return _data[1]; }
+  [[nodiscard]] constexpr float w() const noexcept { return _data[2]; }
+  [[nodiscard]] constexpr float h() const noexcept { return _data[3]; }
+
+  [[nodiscard]] constexpr float& x() noexcept { return _data[0]; }
+  [[nodiscard]] constexpr float& y() noexcept { return _data[1]; }
+  [[nodiscard]] constexpr float& w() noexcept { return _data[2]; }
+  [[nodiscard]] constexpr float& h() noexcept { return _data[3]; }
+
+  [[nodiscard]] constexpr float operator[](std::size_t i) const noexcept {
+    return _data[i];
+  }
+
+  [[nodiscard]] constexpr float& operator[](std::size_t i) noexcept {
+    return _data[i];
+  }
+
+  [[nodiscard]] operator SDL_FRect() const noexcept {
+    return SDL_FRect{
+      .x = _data[0],
+      .y = _data[1],
+      .w = _data[2],
+      .h = _data[3]
+    };
+  }
+
+private:
+  std::array<float, 4> _data{};
+};
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator+(vector<N> const& lhs, vector<N> const& rhs) noexcept {
+  vector<N> result;
+  for (std::size_t i = 0; i < N; ++i) {
+    result[i] = lhs[i] + rhs[i];
+  }
+  return result;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator-(vector<N> const& lhs, vector<N> const& rhs) noexcept {
+  vector<N> result;
+  for (std::size_t i = 0; i < N; ++i) {
+    result[i] = lhs[i] - rhs[i];
+  }
+  return result;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator*(vector<N> const& lhs, float scalar) noexcept {
+  vector<N> result;
+  for (std::size_t i = 0; i < N; ++i) {
+    result[i] = lhs[i] * scalar;
+  }
+  return result;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator*(float scalar, vector<N> const& rhs) noexcept {
+  return rhs * scalar;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator/(vector<N> const& lhs, float scalar) noexcept {
+  vector<N> result;
+  for (std::size_t i = 0; i < N; ++i) {
+    result[i] = lhs[i] / scalar;
+  }
+  return result;
+}
+
+template <std::size_t N>
+constexpr auto& operator+=(vector<N>& lhs, vector<N> const& rhs) noexcept {
+  for (std::size_t i = 0; i < N; ++i) {
+    lhs[i] += rhs[i];
+  }
+  return lhs;
+}
+
+template <std::size_t N>
+constexpr auto& operator-=(vector<N>& lhs, vector<N> const& rhs) noexcept {
+  for (std::size_t i = 0; i < N; ++i) {
+    lhs[i] -= rhs[i];
+  }
+  return lhs;
+}
+
+template <std::size_t N>
+constexpr auto& operator*=(vector<N>& lhs, float scalar) noexcept {
+  for (std::size_t i = 0; i < N; ++i) {
+    lhs[i] *= scalar;
+  }
+  return lhs;
+}
+
+template <std::size_t N>
+constexpr auto& operator/=(vector<N>& lhs, float scalar) noexcept {
+  for (std::size_t i = 0; i < N; ++i) {
+    lhs[i] /= scalar;
+  }
+  return lhs;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr bool operator==(vector<N> const& lhs, vector<N> const& rhs) noexcept {
+  for (std::size_t i = 0; i < N; ++i) {
+    if (lhs[i] != rhs[i]) return false;
+  }
+  return true;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator-(vector<N> const& vec) noexcept {
+  vector<N> result;
+  for (std::size_t i = 0; i < N; ++i) {
+    result[i] = -vec[i];
+  }
+  return result;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr auto operator*(vector<N> const& lhs, vector<N> const& rhs) noexcept {
+  vector<N> result;
+  for (std::size_t i = 0; i < N; ++i) {
+    result[i] = lhs[i] * rhs[i];
+  }
+  return result;
+}
+
+template <std::size_t N>
+constexpr auto& operator*=(vector<N>& lhs, vector<N> const& rhs) noexcept {
+  for (std::size_t i = 0; i < N; ++i) {
+    lhs[i] *= rhs[i];
+  }
+  return lhs;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr float dot(vector<N> const& lhs, vector<N> const& rhs) noexcept {
+  float result = 0.0f;
+  for (std::size_t i = 0; i < N; ++i) {
+    result += lhs[i] * rhs[i];
+  }
+  return result;
+}
+
+template <std::size_t N>
+[[nodiscard]] constexpr float length_squared(vector<N> const& vec) noexcept {
+  return dot(vec, vec);
+}
+
+template <std::size_t N>
+[[nodiscard]] inline float length(vector<N> const& vec) noexcept {
+  return std::sqrt(length_squared(vec));
+}
+
+template <std::size_t N>
+[[nodiscard]] inline auto normalize(vector<N> const& vec) noexcept {
+  return vec / length(vec);
+}
+
+using vec2 = vector<2>;
+using vec4 = vector<4>;
+}
+}
