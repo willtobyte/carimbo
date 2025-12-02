c3fcf73fc9597c16a0a1b8f87235117309620922
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-02 10:03:19 -0300

    Work in progress

--- src/tilemap.hpp
+++ src/tilemap.hpp
@@ -1,81 +1,59 @@
 #pragma once
 
 #include "common.hpp"
-
-struct alignas(16) uv final {
-  float u0, v0;
-  float u1, v1;
-};
-
-struct alignas(16) tileset final {
-  vec2 size;
-  vec2 atlas_size_inv;
-  uint32_t tiles_per_row;
-};
+#include "io.hpp"
+#include "pixmap.hpp"
+#include "pixmappool.hpp"
 
 struct alignas(16) tile final {
-  vec2 position;
-  uint32_t uv_index;
+  float x;
+  float y;
+  uint32_t id;
+
+  friend void from_json(const nlohmann::json& j, tile& t) {
+    j.at("x").get_to(t.x);
+    j.at("y").get_to(t.y);
+    t.id = static_cast<uint32_t>(std::stoul(j.at("id").get<std::string>()));
+  }
 };
 
 struct alignas(64) layer final {
-  std::vector<tile> tiles;
+  bool collider;
   std::string name;
-  bool collider{false};
+  std::vector<tile> tiles;
+
+  friend void from_json(const nlohmann::json& j, layer& l) {
+    j.at("collider").get_to(l.collider);
+    j.at("name").get_to(l.name);
+    j.at("tiles").get_to(l.tiles);
+  }
 };
 
-struct alignas(64) tilemap final {
+class tilemap final {
+public:
+  tilemap(std::string_view name, std::shared_ptr<pixmappool> pixmappool) {
+
+    const auto buffer = io::read(std::format("tilemaps/{}.json", name));
+    const auto j = nlohmann::json::parse(buffer);
+    from_json(j, *this);
+
+    atlas = pixmappool->get(std::format("blobs/tilemaps/{}.png", name));
+  }
+
+  friend void from_json(const nlohmann::json& j, tilemap& t) {
+    j.at("tileSize").get_to(t.tile_size);
+    j.at("mapWidth").get_to(t.map_width);
+    j.at("mapHeight").get_to(t.map_height);
+    j.at("layers").get_to(t.layers);
+  }
+
+private:
+  std::shared_ptr<pixmap> atlas;
   std::vector<SDL_Vertex> vertices;
   std::vector<int32_t> indices;
-  std::vector<uv> table;
   std::vector<layer> layers;
-  tileset atlas;
+  int32_t tile_size;
+  int32_t map_width;
+  int32_t map_height;
+  uint32_t tiles_per_row;
 };
-
-// namespace framework {
-
-// struct transaction final {
-//   std::vector<std::string> path;
-//   uint16_t delay;
-
-//   friend void from_json(const nlohmann::json& j, transaction& t) {
-//     j.at("path").get_to(t.path);
-//     j.at("delay").get_to(t.delay);
-//   }
-// };
-
-// class tilemap final {
-//   public:
-//     tilemap() = delete;
-//     explicit tilemap(
-//       size size,
-//       std::shared_ptr<resourcemanager> resourcemanager,
-//       std::string_view name
-//     );
-//     ~tilemap() = default;
-
-//     void update(float delta);
-//     void draw() const;
-//     void set_target(std::shared_ptr<object> object);
-//     void set_camera(std::shared_ptr<camera> camera);
-
-//     std::vector<std::string> under() const;
-
-//   private:
-//     float _size;
-//     float _height;
-//     float _width;
-//     uint64_t _last_tick{0};
-//     size_t _current_transaction{0};
-//     rectangle _camera;
-
-//     std::shared_ptr<pixmap> _pixmap;
-//     std::vector<rectangle> _sources;
-//     std::vector<std::string> _labels;
-//     std::vector<bool> _visibles;
-//     std::vector<std::vector<uint8_t>> _layers;
-//     std::vector<transaction> _transactions;
-//     std::shared_ptr<object> _target;
-//     // std::shared_ptr<camera> _camera;
-// };
-// }
