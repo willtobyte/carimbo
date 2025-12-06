8db410bdf1cabc229f65bdb1652b4285e09fb4b1
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-06 18:05:00 -0300

    Work in progress

--- src/systems.hpp
+++ src/systems.hpp
@@ -5,39 +5,39 @@
 
 class animationsystem final {
 public:
-  using group_type = decltype(std::declval<entt::registry&>().group<atlas, playback, callbacks>());
-
   explicit animationsystem(entt::registry& registry) noexcept
     : _group(registry.group<atlas, playback, callbacks>()) {}
 
   void update(uint64_t now) noexcept;
 
 private:
+  using group_type = decltype(std::declval<entt::registry&>().group<atlas, playback, callbacks>());
+
   group_type _group;
 };
 
 class physicssystem final {
 public:
-  using group_type = decltype(std::declval<entt::registry&>().group<transform, playback, physics, renderable>());
-
   explicit physicssystem(entt::registry& registry) noexcept
     : _group(registry.group<transform, playback, physics, renderable>()) {}
 
   void update(b2WorldId world, float delta) noexcept;
 
 private:
+  using group_type = decltype(std::declval<entt::registry&>().group<transform, playback, physics, renderable>());
+
   group_type _group;
 };
 
 class rendersystem final {
 public:
-  using group_type = decltype(std::declval<entt::registry&>().group<renderable>(entt::get<transform, tint, sprite, playback, orientation>));
-
   explicit rendersystem(entt::registry& registry) noexcept
     : _group(registry.group<renderable>(entt::get<transform, tint, sprite, playback, orientation>)) {}
 
   void draw() const noexcept;
 
 private:
+  using group_type = decltype(std::declval<entt::registry&>().group<renderable>(entt::get<transform, tint, sprite, playback, orientation>));
+
   group_type _group;
 };
