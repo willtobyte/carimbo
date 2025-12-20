646b79ff18a0fdd09ed02b57de224085cdfd621f
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-20 19:07:32 -0300

    Work in progress

--- src/bootstrap.lua
+++ src/bootstrap.lua
@@ -1,3 +1,16 @@
 R"(
--- ...
+function async(fn)
+  local co = coroutine.create(fn)
+  local function resume()
+    local ok, delay = coroutine.resume(co)
+    if ok and delay and coroutine.status(co) ~= "dead" then
+      timermanager:singleshot(delay, resume)
+    end
+  end
+  resume()
+end
+
+function wait(ms)
+  coroutine.yield(ms)
+end
 )";
