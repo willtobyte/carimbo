00f88f0d48495319e681a61cec0c5744e0612651
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-21 12:19:23 -0300

    Work in progress

--- src/physics.hpp
+++ src/physics.hpp
@@ -2,14 +2,20 @@
 
 #include "common.hpp"
 
-enum collisioncategory : std::uint64_t {
-  none        = 0u,
-  player      = 1u << 0,  // 0x0001
-  enemy       = 1u << 1,  // 0x0002
-  projectile  = 1u << 2,  // 0x0004
-  terrain     = 1u << 3,  // 0x0008
-  trigger     = 1u << 4,  // 0x0010
-  collectible = 1u << 5,  // 0x0020
-  interface   = 1u << 6,  // 0x0040
-  all         = 0xFFFFu
+enum class collisioncategory : std::uint64_t {
+  none        = 0ull,
+  player      = 1ull << 0,
+  enemy       = 1ull << 1,
+  projectile  = 1ull << 2,
+  terrain     = 1ull << 3,
+  trigger     = 1ull << 4,
+  collectible = 1ull << 5,
+  interface   = 1ull << 6,
+  all         = player
+              | enemy
+              | projectile
+              | terrain
+              | trigger
+              | collectible
+              | interface
 };
