8937bb4d047f50a8ac46605144750ac01b8bd4fd
Rodrigo Delduca <rodrigo@delduca.org>
2026-01-19 17:05:50 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,14 @@
 #include "application.hpp"
 
 int main(int argc, char **argv) {
-#ifdef NDEBUG
-#ifndef EMSCRIPTEN
-#ifndef DEVELOPMENT
-  auto* out = std::freopen("stdout.txt", "w", stdout);
-  if (out) setvbuf(out, nullptr, _IONBF, 0);
+#if defined(NDEBUG) && !defined(EMSCRIPTEN) && !defined(DEVELOPMENT)
+  if (auto* out = std::freopen("stdout.txt", "w", stdout)) {
+    setvbuf(out, nullptr, _IONBF, 0);
+  }
 
-  auto* err = std::freopen("stderr.txt", "w", stderr);
-  if (err) setvbuf(err, nullptr, _IONBF, 0);
-#endif
-#endif
+  if (auto* err = std::freopen("stderr.txt", "w", stderr)) {
+    setvbuf(err, nullptr, _IONBF, 0);
+  }
 #endif
 
   application app(argc, argv);
